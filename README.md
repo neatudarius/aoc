dbb93e0866180e51cdd4614d75bdd1e25e5a520c
Darius Neatu <dneatu@bitdefender.com>
2020-12-07 11:42:08 +0200

    tweaks: activate github actions

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# aoc
+# AOC 2020
+
+## skel generator
+
+```
+aoc/2020$ skel/init.sh day01
+
+aoc/2020$ tree day01
+day01
+├── aoc.cpp
+├── aoc.py
+├── in
+├── Makefile -> ../skel/Makefile
+└── out
+
+0 directories, 5 files
+```
--- skel/init.sh
+++ skel/init.sh
@@ -5,11 +5,11 @@ mkdir -p "${DAY_DIR}"
 
 # C++ skel
 cd "${DAY_DIR}" || exit 1
-ln -fs ../skel/Makefile .
-cp ../skel/skel.cpp aoc.cpp
+ln -fs ../../skel/Makefile .
+cp ../../skel/skel.cpp aoc.cpp
 
 # Python skel
-cp ../skel/skel.py aoc.py
+cp ../../skel/skel.py aoc.py
 
 # in/out files
 touch in out
